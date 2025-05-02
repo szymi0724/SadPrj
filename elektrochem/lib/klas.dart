98b9c9e82685c8a043d53fc1d4e361bab8d227f0
lojson1 <141033547+lojson1@users.noreply.github.com>
2025-05-02 12:48:34 +0200

    poprawa gita

---
+++ elektrochem/lib/klas.dart
@@ -0,0 +1,25 @@
+class Data {
+  Data(this.x, this.y);
+
+  final double x;
+  final double y;
+}
+
+class DDItem {
+  final String label;
+  final int value;
+
+  DDItem({required this.label, required this.value});
+}
+
+class PlotSet {
+  int minx;
+  int maxx;
+  int miny;
+  int maxy;
+  String ptype;
+  int cxnum;
+  int cynum;
+
+  PlotSet({required this.minx, required this.maxx, required this.miny, required this.maxy, required this.ptype, required this.cxnum, required this.cynum});
+}
